c23debc125ae4adab08106a09694488ac270941c
Balu-Menon <balbbrl@gmail.com>
2015-05-17 10:42:02 +0530

    first push

---
+++ register.php
@@ -0,0 +1,77 @@
+<?php
+$con=mysqli_connect("localhost","root","","twm");
+// Check connection
+if (mysqli_connect_errno()) {
+	$temp['success'] = 2;
+    $temp['error'] = mysqli_connect_error();
+    echo json_encode($temp);
+}
+
+//validating email	
+if (isset($_POST['email'])) {
+        $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $temp['success'] = 2;
+            $temp['error'] = "Error! Email not valid!!";
+            echo json_encode($temp);
+            }
+          
+	else {
+		$sql="SELECT * FROM register WHERE email='$email'";
+		$result = mysqli_query($con,$sql);
+		$num_rows=mysqli_num_rows($result);
+		if($num_rows>0) { 
+			$temp['success'] = 2;
+            $temp['error'] = "Email already exists!!";
+            echo json_encode($temp);
+			}
+		else {    
+			$name = stripslashes($_POST['name']);
+			$college = stripslashes($_POST['college']);
+			$branch = stripslashes($_POST['branch']);
+			$email= stripslashes($_POST['email']);
+			$mob = stripslashes($_POST['mob']);
+			if(isset($_POST['event1'])){
+				$e1=2;
+			}
+			else {
+				$e1=1;
+			}
+			if(isset($_POST['event2'])){
+				$e2=2;
+			}
+			else {
+				$e2=1;
+			}
+			if(isset($_POST['event3'])){
+				$e3=2;
+			}
+			else {
+				$e3=1;
+			}
+			if(isset($_POST['event4'])){
+				$e4=2;
+			}
+			else {
+				$e4=1;
+			}
+			if(isset($_POST['event5'])){
+				$e5=2;
+			}
+			else {
+				$e5=1;
+			}
+
+			$sql="INSERT INTO register (name,branch,college,email,mob,singleboard,signalp,fgpa,micro,eda) VALUES ('$name', '$branch','$college','$email','$mob','$e1','$e2','$e3','$e4', '$e5')";
+			if (!mysqli_query($con,$sql)) {
+				echo "Server busy. Please try again later.";
+			}
+			else {
+				echo "Successfully Registered.";
+			}
+ 	    }
+	}
+}
+mysqli_close($con);
+
+?>
